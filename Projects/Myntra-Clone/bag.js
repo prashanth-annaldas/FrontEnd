e5731fd34c6a9f41c962a08f6c508a6e656b7c5c
prashanth <prashanthannaldas453@gmail.com>
2025-06-03 16:03:26 +0530

    init

--- Projects/Myntra-Clone/bag.js
+++ Projects/Myntra-Clone/bag.js
@@ -1,9 +1,11 @@
+const CONVENIENCE_FEES = 99;
 let bagItemsObjects;
 onLoad();
 
 function onLoad(){
     loadBagItemObjects();
     displayBagItems();
+    bagSummaryDetails();
 }
 
 function loadBagItemObjects() {
@@ -34,6 +36,13 @@ function displayBagItems(){
                                             <span class="originalPrice">${item.current_price}</span>
                                             <span class="discount">${item.discount}% OFF</span>
                                         </div>
+                                        <div class="returnPeriod">
+                                            <span class="returnPeriodDays">${item.return_period} days</span> return available
+                                        </div>
+                                        <div class="deliveryDetails">
+                                            Delivery by
+                                            <span class="deliveryDetailsDays">${item.delivery_date}</span>
+                                        </div>
                                     </div>
                                     <div class="remove_item" onclick="removeItem(${item.id})">X</div>
                                 </div>`;
@@ -49,4 +58,50 @@ function removeItem(itemId){
     loadBagItemObjects();
     displayBagCount();
     displayBagItems();
+    bagSummaryDetails();
+}
+function bagSummaryDetails(){
+
+    let bagSummaryElements = document.querySelector('.bagSummary');
+    
+    if(bagItemsObjects.length === 0){
+        bagSummaryElements.innerHTML = '';
+        return ;
+    }
+
+    let totalItem = bagItemsObjects.length;
+    let totalMRP = 0;
+    let totalDiscount = 0;
+
+    bagItemsObjects.forEach(bagItem=>{
+        totalMRP += bagItem.original_price;
+        totalDiscount += bagItem.original_price - bagItem.current_price;
+    });
+
+    let finalPayment = totalMRP - totalDiscount + CONVENIENCE_FEES;
+
+    let innerHTML = `<div class="bag-details-container">
+            <div class="price-header">PRICE DETAILS (${totalItem} Items) </div>
+            <div class="price-item">
+                <span class="price-item-tag">Total MRP</span>
+                <span class="price-item-value">₹${totalMRP}</span>
+            </div>
+            <div class="price-item">
+                <span class="price-item-tag">Discount on MRP</span>
+                <span class="price-item-value priceDetail-base-discount">-₹${totalDiscount}</span>
+            </div>
+            <div class="price-item">
+                <span class="price-item-tag">Convenience Fee</span>
+                <span class="price-item-value">₹99</span>
+            </div>
+            <hr>
+            <div class="price-footer">
+                <span class="price-item-tag">Total Amount</span>
+                <span class="price-item-value">₹${finalPayment}</span>
+            </div>
+        </div>
+        <button class="btn-place-order">
+            <div class="css-xjhrni">PLACE ORDER</div>
+        </button>`;
+    bagSummaryElements.innerHTML = innerHTML;
 }
