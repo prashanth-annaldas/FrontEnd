f2d55b88b88d7590e1d0302c70923e49d84b33f8
prashanth <prashanthannaldas453@gmail.com>
2025-06-30 02:09:20 +0530

    init

--- JavaScript/This/arrow_Function.js
+++ JavaScript/This/arrow_Function.js
@@ -11,10 +11,23 @@
 // }
 // b();
 
-const obj = {
+// const obj = {
+//     a:24,
+//     x:()=>{
+//         console.log(this);   // window
+//     }
+// };
+// obj.x();
+
+const obj1 = {
     a:24,
-    x:()=>{
-        console.log(this);   // window
-    }
-}
-obj.x();
+    x:function (){
+        const obj2 = {
+            y:()=>{
+                console.log(this);  // this gives us like the object of the lexical context i.e obj1
+            }
+        }
+        obj2.y();
+    },
+};
+obj1.x();
