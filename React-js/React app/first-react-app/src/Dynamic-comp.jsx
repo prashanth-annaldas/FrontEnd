8711b7f892321b0999cb12ebf0d804d9ceecd136
prashanth <prashanthannaldas453@gmail.com>
2025-07-16 00:35:55 +0530

    init

--- React-js/React app/first-react-app/src/Dynamic-comp.jsx
+++ React-js/React app/first-react-app/src/Dynamic-comp.jsx
@@ -1,9 +1,10 @@
 function Dynamic(){
-    let name = 'Prashanth';
-    let fullName = () =>{
-        return 'Prashanth Annaldas';
+    let name = 'Master Coding';
+    let subscribers = () =>{
+        let sub = Math.random()*100000;
+        return Math.round(sub);
     }
-    return <h2>My name is {name} <br></br> My full name is {fullName()}</h2>;
+    return <p style={{'background-color':'#5CB3FF'}}>channel name is {name} <br></br> No of subscribers : {subscribers()}</p>;
 }
 
 export default Dynamic;
