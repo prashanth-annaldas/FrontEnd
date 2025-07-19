9966593f07cb357d42ab84445c484e5022367e1c
prashanth <prashanthannaldas453@gmail.com>
2025-07-19 18:21:50 +0530

    Name changed

--- React-js/Projectss/Indian-clock-version-1/src/components/Moto.jsx
+++
@@ -1,5 +0,0 @@
-function Moto() {
-  return <p>This is the clock that shows the time in Indian at all times.</p>;
-}
-
-export default Moto;
--- React-js/Projectss/Indian-clock-version-1/src/components/Time.jsx
+++
@@ -1,7 +0,0 @@
-function Time() {
-    let time = new Date();
-  return <p>This is shows time: {time.toLocaleDateString()} - {time.toLocaleTimeString()}</p>;
-
-}
-
-export default Time;
