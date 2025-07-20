2bd6b4a1f77b58a4b52b4a36bf256bac92f62421
prashanth <prashanthannaldas453@gmail.com>
2025-07-20 17:27:22 +0530

    Added Passing Children

--- React-js/Fragments/Learning-Fragment/src/Map.jsx
+++ React-js/Fragments/Learning-Fragment/src/Map.jsx
@@ -1,16 +1,17 @@
-import FoodItems from './components/listItems';
+import ClothItems from './components/listItems';
 import ErrorMsg from './components/errMessage';
+import Container from './components/Container';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 function Map() {
     // let listItems = [];
     let listItems = ["Pants","Shoes","Shirts","T-Shirts","Kurta","shorts"];
 
-  return  (<>
-              <h1>Shop (Using Map)</h1>
+  return  (<Container>
+              <h1>Online Store</h1>
               <ErrorMsg items = {listItems}/>
-              <FoodItems items = {listItems}/>
-           </>
+              <ClothItems items = {listItems}/>
+           </Container>
   );
 }
  
--- React-js/Fragments/Learning-Fragment/src/components/Item.jsx
+++ React-js/Fragments/Learning-Fragment/src/components/Item.jsx
@@ -1,5 +1,11 @@
 const Item = ({clothItems})=>{
-    return <li className="list-group-item item">{clothItems}</li>;
+    const handleButtonClicked = (clothItems)=>{
+        console.log(`${clothItems} being bought`)
+    }
+    return <li className="list-group-item">
+        <span className="item">{clothItems}</span>
+        <button className="Button  btn btn-info" onClick={()=>handleButtonClicked(clothItems)}>Buy</button>
+    </li>
 }
 
 export default Item;
