6e4809e1a1c4938861b54f0f88476f79cb582151
prashanth <prashanthannaldas453@gmail.com>
2025-07-22 23:19:44 +0530

    Updated Handling Events

--- React-js/Fragments/Learning-Fragment/src/components/listItems.jsx
+++ React-js/Fragments/Learning-Fragment/src/components/listItems.jsx
@@ -3,7 +3,8 @@ import Item from './Item';
 const foodItems = ({items}) =>{
     return (<ul className="list-group">
                 {items.map((item) =>(
-                    <Item key={item} clothItems={item}/>
+                    <Item key={item} clothItems={item} handleButtonClicked = {()=>{console.log(`${item} being bought`)}
+    }/>
                 ))}
               </ul>);
 }
