19bf2db4bd32cfae7183fbdfb14972010a42897f
prashanth <prashanthannaldas453@gmail.com>
2025-07-22 23:21:49 +0530

    updated

--- React-js/Fragments/Learning-Fragment/src/Map.jsx
+++ React-js/Fragments/Learning-Fragment/src/Map.jsx
@@ -1,6 +1,7 @@
-import ClothItems from './components/listItems';
+import StoreItems from './components/listItems';
 import ErrorMsg from './components/errMessage';
 import Container from './components/Container';
+import StoreInput from './components/StoreInput';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 function Map() {
@@ -9,8 +10,11 @@ function Map() {
 
   return  (<Container>
               <h1>Online Store</h1>
+              <StoreInput handleOnChange = {(event)=>{ 
+        console.log(event.target.value);}
+    }/>
               <ErrorMsg items = {listItems}/>
-              <ClothItems items = {listItems}/>
+              <StoreItems items = {listItems}/>
            </Container>
   );
 }
