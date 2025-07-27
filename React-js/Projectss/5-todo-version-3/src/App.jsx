9d9aa5ef9eb474864f09c5addcbaa0391508b84e
prashanth <prashanthannaldas453@gmail.com>
2025-07-28 01:04:09 +0530

    Updated App Dynamically

--- React-js/Projectss/5-todo-version-3/src/App.jsx
+++ React-js/Projectss/5-todo-version-3/src/App.jsx
@@ -1,6 +1,7 @@
 import AppName from "./components/AppName";
 import SearchBar from "./components/Search"
 import TodoItem from "./components/todo-items"
+import WelcomeMsg from "./components/WelcomeMsg";
 import "./App.css"
 import { useState } from "react";
 
@@ -24,17 +25,24 @@ function App() {
     },
   ];
 
-  const [todoItems,setTodoItems] = useState(initializeTodoItems);
+  const [todoItems,setTodoItems] = useState([]);
 
 
-  function handleButton(itemName, itemDueDate){
-    console.log(`This is the ${itemName} and due date is ${itemDueDate}`);
+  function handleNewButton(itemName, itemDueDate){
+    let newTodoItems = [...todoItems,{name: itemName,date: itemDueDate}]
+    setTodoItems(newTodoItems);
+  }
+
+  function handleDeleteButton(todoItemName){
+    let newTodoItem = todoItems.filter((item)=>item.name !== todoItemName);
+    setTodoItems(newTodoItem);
   }
 
   return (<center className="todo-container">
             <AppName/>
-              <SearchBar onNewItems={handleButton}/>
-              <TodoItem todoItems={todoItems}/>
+              <SearchBar onNewItems={handleNewButton}/>
+              {todoItems.length === 0 && <WelcomeMsg/>}
+              <TodoItem todoItems={todoItems} onDeleteItems = {handleDeleteButton}/>
          </center>);
 }
 
