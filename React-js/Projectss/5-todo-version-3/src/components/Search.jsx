a9c8ad9ccb642ab99fa047376cd9dd3da7487d72
prashanth <prashanthannaldas453@gmail.com>
2025-07-27 23:56:53 +0530

    Added Dynamic Search

--- React-js/Projectss/5-todo-version-3/src/components/Search.jsx
+++ React-js/Projectss/5-todo-version-3/src/components/Search.jsx
@@ -1,12 +1,30 @@
-function SearchBar({onNewItems}){
+import { useState } from "react";
 
-    return (<div className="container text-center">
-              <div className="row n-row">
-                <div className="col-6"><input type="text" placeholder="Enter Todo Here"/></div>
-                <div className="col-4"><input type="date" /></div>
-                <div className="col-1"><button type="button" className="btn btn-success n-button" onClick={()=>onNewItems("p","q")}>Add</button></div>
-              </div>
-            </div>);
+function SearchBar({onNewItems}){
+  const [todoName,setTodoName] = useState();
+  const [todoDate,setTodoDate] = useState();
+
+  function handleNameChange(event){
+    setTodoName(event.target.value);
+  }
+  
+  function handleDateChange(event){
+    setTodoDate(event.target.value);
+  }
+
+  function handleButtonClicked(){
+    onNewItems(todoName,todoDate);
+    setTodoName("");
+    setTodoDate("");
+  }
+
+  return (<div className="container text-center">
+            <div className="row n-row">
+              <div className="col-6"><input type="text" value={todoName} placeholder="Enter Todo Here" onChange={handleNameChange}/></div>
+              <div className="col-4"><input type="date" value={todoDate} onChange={handleDateChange}/></div>
+              <div className="col-1"><button type="button" className="btn btn-success n-button" onClick={handleButtonClicked}>Add</button></div>
+            </div>
+          </div>);
 }
 
 export default SearchBar;
