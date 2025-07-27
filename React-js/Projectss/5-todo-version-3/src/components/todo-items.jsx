2257477d14b9301f74a55c4ce461fba997dc05aa
prashanth <prashanthannaldas453@gmail.com>
2025-07-28 01:02:27 +0530

    Updated Delete Items

--- React-js/Projectss/5-todo-version-3/src/components/todo-items.jsx
+++ React-js/Projectss/5-todo-version-3/src/components/todo-items.jsx
@@ -1,10 +1,10 @@
 import TodoItem from "./todo-item"
 import styles from "./todo-items.module.css";
 
-function TodoItems({todoItems}){
+function TodoItems({todoItems,onDeleteItems}){
     return <div className={styles.itemsContainer}>
                 {todoItems.map((item) =>(
-                    <TodoItem todoName={item.name} todoDate={item.date}/>
+                    <TodoItem todoName={item.name} todoDate={item.date} todoDelete = {onDeleteItems}/>
                 ))}
             </div>;
 }
