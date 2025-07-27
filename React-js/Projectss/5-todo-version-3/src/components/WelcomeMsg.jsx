43c1dad3d2ed60a79be604d52a024e287dc75114
prashanth <prashanthannaldas453@gmail.com>
2025-07-28 01:02:47 +0530

    Welcome Message

---
+++ React-js/Projectss/5-todo-version-3/src/components/WelcomeMsg.jsx
@@ -0,0 +1,6 @@
+import styles from './WelcomeMsg.module.css'
+function WelcomeMsg(){
+    return <h2 className={styles.WelcomeMsg}>Enjoy your Day, keep smiling :)</h2>
+}
+
+export default WelcomeMsg;
