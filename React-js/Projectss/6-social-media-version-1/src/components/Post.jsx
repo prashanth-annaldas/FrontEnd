3601bc1e93f44c2cbfeca14fde2c19a9b967a280
prashanth <prashanthannaldas453@gmail.com>
2025-08-03 00:38:34 +0530

    Updated delete Post

--- React-js/Projectss/6-social-media-version-1/src/components/Post.jsx
+++ React-js/Projectss/6-social-media-version-1/src/components/Post.jsx
@@ -1,11 +1,21 @@
-import PostList from "./PostList";
+import { useContext } from "react";
+import { MdDelete } from "react-icons/md";
+import { PostList } from "../store/post-list-store";
 
 function Post({post}){
-    return <div className="card post" style={{width: "18rem"}}>
+    const {deletePost} = useContext(PostList);
+    return <div className="card post" style={{width: "30rem"}}>
                 <div className="card-body">
-                    <h5 className="card-title">{post.heading}</h5>
+                    <h5 className="card-title">{post.heading}
+                        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" onClick={()=>deletePost(post.id)}>
+                            <MdDelete />
+                        </span>
+                    </h5>
                     <p className="card-text">{post.body}</p>
-                    {post.hashTags.map((tags)=><span class="badge text-bg-primary hashTag">{tags}</span>)}
+                    {post.hashTags.map((tags)=><span key={tags} className="badge text-bg-primary hashTag">{tags}</span>)}
+                    <div className="alert alert-success reaction" role="alert">
+                        This post has been reacted {post.postReactions} people.
+                    </div>
                 </div>
             </div>
 }
