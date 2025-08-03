7fd13f3912d8c6537cd1bafa275ed04ac16a5507
prashanth <prashanthannaldas453@gmail.com>
2025-08-04 00:20:16 +0530

    Get Posts From Server

--- React-js/Projectss/6-social-media-version-1/src/components/Post.jsx
+++ React-js/Projectss/6-social-media-version-1/src/components/Post.jsx
@@ -6,15 +6,15 @@ function Post({post}){
     const {deletePost} = useContext(PostList);
     return <div className="card post" style={{width: "30rem"}}>
                 <div className="card-body">
-                    <h5 className="card-title">{post.heading}
+                    <h5 className="card-title">{post.title}
                         <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" onClick={()=>deletePost(post.id)}>
                             <MdDelete />
                         </span>
                     </h5>
                     <p className="card-text">{post.body}</p>
-                    {post.hashTags.map((tags)=><span key={tags} className="badge text-bg-primary hashTag">{tags}</span>)}
+                    {post.tags.map((tags)=><span key={tags} className="badge text-bg-primary hashTag">{tags}</span>)}
                     <div className="alert alert-success reaction" role="alert">
-                        This post has been reacted {post.postReactions} people.
+                        This post has been reacted {post.views} people.
                     </div>
                 </div>
             </div>
