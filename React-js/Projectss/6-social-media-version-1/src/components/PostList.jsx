212829de577baa26a5b7fe501ce7d85dded1de12
prashanth <prashanthannaldas453@gmail.com>
2025-08-04 00:20:29 +0530

    Updated

--- React-js/Projectss/6-social-media-version-1/src/components/PostList.jsx
+++ React-js/Projectss/6-social-media-version-1/src/components/PostList.jsx
@@ -1,10 +1,21 @@
 import { useContext } from 'react';
 import Post from './Post'
 import {PostList as PostListData} from '../store/post-list-store'
+import WelcomeMsg from './WelcomeMsg';
 
 function PostList(){
-    const {postList} = useContext(PostListData);
+    const {postList, addInitialPosts} = useContext(PostListData);
+
+    const handleGetPostsClicked = ()=>{
+        fetch('https://dummyjson.com/posts')
+        .then(res => res.json())
+        .then((data)=>{
+            addInitialPosts(data.posts);
+        })
+    }
+
     return <>
+            {postList.length === 0 && <WelcomeMsg  onGetPostsClicked = {handleGetPostsClicked}/>}
             {postList.map((post)=>(<Post key = {post.id} post = {post}/>))};
         </>
 }
