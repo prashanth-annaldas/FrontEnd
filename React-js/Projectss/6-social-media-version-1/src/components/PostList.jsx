39170f9f0edde6eb6cdd69e57f9373e884de837d
prashanth <prashanthannaldas453@gmail.com>
2025-08-05 00:12:18 +0530

    Addded useEffect

--- React-js/Projectss/6-social-media-version-1/src/components/PostList.jsx
+++ React-js/Projectss/6-social-media-version-1/src/components/PostList.jsx
@@ -1,22 +1,26 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Post from './Post'
 import {PostList as PostListData} from '../store/post-list-store'
 import WelcomeMsg from './WelcomeMsg';
+import LoadingSpinner from './LoadingSpinner'
 
 function PostList(){
     const {postList, addInitialPosts} = useContext(PostListData);
-
-    const handleGetPostsClicked = ()=>{
+    const [fetching,setFetching] = useState(false);
+    useEffect(()=>{
+        setFetching(true);
         fetch('https://dummyjson.com/posts')
-        .then(res => res.json())
-        .then((data)=>{
-            addInitialPosts(data.posts);
-        })
-    }
+            .then(res => res.json())
+            .then((data)=>{
+                addInitialPosts(data.posts);
+                setFetching(false);
+            })
+    },[]);
 
     return <>
-            {postList.length === 0 && <WelcomeMsg  onGetPostsClicked = {handleGetPostsClicked}/>}
-            {postList.map((post)=>(<Post key = {post.id} post = {post}/>))};
+            {fetching && <LoadingSpinner/>}
+            {!fetching && postList.length === 0 && <WelcomeMsg/>}
+            {!fetching && postList.map((post)=>(<Post key = {post.id} post = {post}/>))};
         </>
 }
 
