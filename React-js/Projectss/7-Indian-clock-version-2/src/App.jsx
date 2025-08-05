06acc2e26a1c73d5650a2518321244ac0f5154de
prashanth <prashanthannaldas453@gmail.com>
2025-08-05 23:48:11 +0530

    Indian Clock Version - 2

---
+++ React-js/Projectss/7-Indian-clock-version-2/src/App.jsx
@@ -0,0 +1,15 @@
+import Heading from './components/Heading'
+import Moto from './components/Moto'
+import Time from './components/Time'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return <center>
+    <Heading/>
+    <Moto/>
+    <Time/>
+  </center>
+}
+
+export default App
