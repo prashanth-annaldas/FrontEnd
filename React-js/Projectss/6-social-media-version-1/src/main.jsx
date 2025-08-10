0c81c9096ca61fefba07df12ed3f3717591a157a
prashanth <prashanthannaldas453@gmail.com>
2025-08-10 19:44:02 +0530

    Added Router

--- React-js/Projectss/6-social-media-version-1/src/main.jsx
+++ React-js/Projectss/6-social-media-version-1/src/main.jsx
@@ -1,9 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from './App.jsx';
+import CreatePost from './components/CreatePost.jsx';
+import PostList from './components/PostList.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path:"/", 
+    element:<App />,
+    children:[
+      {path:"/", element: <PostList />},
+      {path:"/create-post", element: <CreatePost />}
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router = {router}/>
+  </React.StrictMode>
 )
