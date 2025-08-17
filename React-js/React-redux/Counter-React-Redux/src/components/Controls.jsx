c444949681e0f955f31dc09194a7a55fef1a540d
prashanth <prashanthannaldas453@gmail.com>
2025-08-17 16:23:20 +0530

    update

--- React-js/React-redux/Counter-React-Redux/src/components/Controls.jsx
+++ React-js/React-redux/Counter-React-Redux/src/components/Controls.jsx
@@ -1,8 +1,10 @@
+import { useRef } from "react";
 import { useDispatch } from "react-redux";
 
 function controls(){
 
     const dispatch = useDispatch();
+    const inputElement = useRef();
 
     const handleIncrementButton = ()=>{
         dispatch({type: 'INCREMENT'})
@@ -12,20 +14,70 @@ function controls(){
         dispatch({type: 'DECREMENT'})
     }
 
-    return <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                <button 
-                    type="button"
-                    className="btn btn-success"
-                    onClick={handleIncrementButton}>
-                    +1
-                </button>
-                <button 
-                    type="button" 
-                    className="btn btn-danger"
-                    onClick={handleDecrementButton}>
-                    -1
-                </button>
-            </div>
+    const handleAdditionButton = ()=>{
+        dispatch({
+            type: 'ADD',
+            payload:{
+                number:inputElement.current.value
+            },
+        });
+        inputElement.current.value = "";
+    }
+
+    const handleSubtractButton = ()=>{
+        dispatch({
+            type: 'SUBTRACT',
+            payload:{
+                number:inputElement.current.value
+            },
+        });
+        inputElement.current.value = "";
+    }
+
+    const handlePrivacyToggle = ()=>{
+        dispatch({
+            type: 'PRIVACY_TOGGLE',
+        });
+    }
+
+    return  <>
+                <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                    <button 
+                        type="button"
+                        className="btn btn-success"
+                        onClick={handleIncrementButton}>
+                        +1
+                    </button>
+                    <button
+                        type="button" 
+                        className="btn btn-danger"
+                        onClick={handleDecrementButton}>
+                        -1
+                    </button>
+                    <button 
+                        type="button" 
+                        class="btn btn-warning"
+                        onClick={handlePrivacyToggle}>
+                        Privacy Toggle
+                    </button>
+                </div>
+
+                <div className="d-grid gap-2 d-sm-flex justify-content-sm-center control-row">
+                    <input type="text" placeholder="Enter number" className="number-input" ref={inputElement}/>
+                    <button
+                        type="button"
+                        className="btn btn-success"
+                        onClick={handleAdditionButton}>
+                        Add
+                    </button>
+                    <button 
+                        type="button" 
+                        className="btn btn-danger"
+                        onClick={handleSubtractButton}>
+                        Subtract
+                    </button>
+                </div>
+            </>
 }
 
 export default controls;
