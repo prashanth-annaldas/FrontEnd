1511e4766225349b8bf5f0f2e716ba2b101a5229
prashanth <prashanthannaldas453@gmail.com>
2025-08-19 23:58:58 +0530

    Update

--- React-js/Projectss/8-Myntra-Clone/3-myntra-react-clone/src/components/Header.jsx
+++ React-js/Projectss/8-Myntra-Clone/3-myntra-react-clone/src/components/Header.jsx
@@ -2,12 +2,13 @@ import { IoMdPerson } from "react-icons/io";
 import { MdFavoriteBorder } from "react-icons/md";
 import { IoBag } from "react-icons/io5";
 import { IoIosSearch } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 function Header(){
     return  <>
                 <header>
                     <div className="logo_container">
-                        <a href="#"><img className="myntra_home" src="/../images/myntra_logo.webp" alt="Myntra Home" /></a>
+                        <Link to="/"><img className="myntra_home" src="/../images/myntra_logo.webp" alt="Myntra Home" /></Link>
                     </div>
                     <nav className="nav_bar">
                         <a href="#">Men</a>
@@ -32,11 +33,11 @@ function Header(){
                             <span className="action_name">Wishlist</span>
                         </div>
 
-                        <div className="action_container">
+                        <Link className="action_container" to="/bag">
                             <IoBag />
                             <span className="action_name">Bag</span>
                             <span className="bag-item-count">0</span>
-                        </div>
+                        </Link>
                     </div>
                 </header>
             </>
