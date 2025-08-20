de5bf0a741cb3b80838e4d624e2beb5729bd76c5
prashanth <prashanthannaldas453@gmail.com>
2025-08-20 14:51:32 +0530

    Update

--- React-js/Projectss/8-Myntra-Clone/3-myntra-react-clone/src/components/Header.jsx
+++ React-js/Projectss/8-Myntra-Clone/3-myntra-react-clone/src/components/Header.jsx
@@ -3,8 +3,12 @@ import { MdFavoriteBorder } from "react-icons/md";
 import { IoBag } from "react-icons/io5";
 import { IoIosSearch } from "react-icons/io";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header(){
+
+    const bag = useSelector((store) => store.bag);
+    console.log(bag);
     return  <>
                 <header>
                     <div className="logo_container">
@@ -36,7 +40,7 @@ function Header(){
                         <Link className="action_container" to="/bag">
                             <IoBag />
                             <span className="action_name">Bag</span>
-                            <span className="bag-item-count">0</span>
+                            <span className="bag-item-count">{bag.length}</span>
                         </Link>
                     </div>
                 </header>
