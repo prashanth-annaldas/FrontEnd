8a930476bb9fd9e6ba3e71eb7bafc01291249987
prashanth <prashanthannaldas453@gmail.com>
2025-08-20 14:52:15 +0530

    Update

--- React-js/Projectss/8-Myntra-Clone/3-myntra-react-clone/src/store/index.js
+++ React-js/Projectss/8-Myntra-Clone/3-myntra-react-clone/src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import itemsSlice from "./itemsSlice";
 import fetchStatusSlice from "./FetchStatusSlice";
+import bagSlice from "./BagSlice";
 
 const myntraStore = configureStore({
     reducer:{
         items:itemsSlice.reducer,
         fetchStatus: fetchStatusSlice.reducer,
+        bag: bagSlice.reducer,
     }
 });
 
