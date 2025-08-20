94f1b4fe542bfa587fc7e64c7bde60198fccf232
prashanth <prashanthannaldas453@gmail.com>
2025-08-20 14:51:54 +0530

    Update

--- React-js/Projectss/8-Myntra-Clone/3-myntra-react-clone/src/routes/Bag.jsx
+++ React-js/Projectss/8-Myntra-Clone/3-myntra-react-clone/src/routes/Bag.jsx
@@ -1,27 +1,20 @@
+import { useSelector } from "react-redux";
 import BagItems from "../components/BagItems";
 import BagSummary from "../components/BagSummary";
 
 function bag() {
-  const item = {
-    id: "001",
-    image: "/images/1.jpg",
-    company: "Carlton London",
-    item_name: "Rhodium-Plated CZ Floral Studs",
-    original_price: 1045,
-    current_price: 606,
-    discount_percentage: 42,
-    return_period: 14,
-    delivery_date: "10 Oct 2023",
-    rating: {
-      stars: 4.5,
-      count: 1400,
-    },
-  };
+  const bagItems = useSelector((store) => store.bag);
+  const items = useSelector((store) => store.items);
+  const finalItems = items.filter((item) => {
+    const itemIndex = bagItems.indexOf(item.id) >= 0;
+    return itemIndex;
+  });
+
   return (
     <main>
       <div className="bag-page">
         <div className="bag-items-container">
-          <BagItems item = { item }/>
+          {finalItems.map((item) => <BagItems item={item} />)}
         </div>
         <div className="bag-summary">
           <BagSummary />
