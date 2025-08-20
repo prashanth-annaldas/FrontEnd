2e5f8d26fd52e14ffdc738eed4a6b1858fb058aa
prashanth <prashanthannaldas453@gmail.com>
2025-08-20 14:51:49 +0530

    Update

--- React-js/Projectss/8-Myntra-Clone/3-myntra-react-clone/src/routes/App.jsx
+++ React-js/Projectss/8-Myntra-Clone/3-myntra-react-clone/src/routes/App.jsx
@@ -2,13 +2,18 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
 import FetchItems from "../components/FetchItems";
+import LoadingSpinner from "../components/LoadingSpinner";
+import { useSelector } from "react-redux";
 
 function App() {
+
+  const fetchStatus = useSelector((store)=>store.fetchStatus);
+
   return (
     <>
       <Header />
       <FetchItems />
-      <Outlet />
+      {fetchStatus.currentlyFetching ? <LoadingSpinner /> : <Outlet />}
       <Footer />
     </>
   );
