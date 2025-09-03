e11b8899ae86baee27e2221f1897447d30872499
prashanth <prashanthannaldas453@gmail.com>
2025-09-04 01:48:01 +0530

    Mini-Netflix-App

---
+++ React-js/Mini-Netflix-App/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import './App.css'
+import Input from './components/Input';
+import ErrorMsg from './components/ErrorMsg';
+import Display from './components/display';
+
+function App() {
+  const [movieName, setMovieName] = useState("");
+  const [movie, setMovie] = useState(null);
+  const [error, setError] = useState("");
+  
+  const API_URL = "https://www.omdbapi.com/?apikey=acc614ce"
+
+  const handleFetching = async (e)=>{
+    e.preventDefault();
+    if(!movieName.trim()){
+      setError("Enter any movie name");
+      return ;
+    }
+    setMovie(null);
+    setError("");
+    try{
+      setError("");
+      const res = await fetch(`${API_URL}&t=${movieName}`);
+      const data = await res.json();
+
+      if(data.Response === "True"){
+        setMovie(data);
+        setMovieName("");
+      }
+      else{
+        setError(data.Error || "Movie Not Found!!");
+      }
+    }
+    catch(err){
+      setError(err.message || "Failed to fetch Movie");
+    }
+  }
+
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <Input movieName={ movieName } setMovieName={ setMovieName } handleFetching={ handleFetching } />
+      { error && <ErrorMsg error={ error } /> }
+      { movie && <Display movie={ movie } /> }
+    </div>
+  )
+}
+
+export default App
