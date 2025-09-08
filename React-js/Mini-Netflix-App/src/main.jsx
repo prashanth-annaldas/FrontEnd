2488820e399358630a45c31cfa3a354f5bdf3daf
prashanth <prashanthannaldas453@gmail.com>
2025-09-08 18:26:42 +0530

    Update

--- React-js/Mini-Netflix-App/src/main.jsx
+++ React-js/Mini-Netflix-App/src/main.jsx
@@ -1,9 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import Wishlist from './components/wishlist.jsx';
+import Display from './components/wishlist.jsx';
+import netflixStore from './store/index.js'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path:"/",element:<App />,
+    children:[
+      { path: "/", element: <Display /> },
+      { path: "/bag", element: <Wishlist /> }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store = { netflixStore }>
+      <RouterProvider router = {router} />
+    </Provider>
   </StrictMode>,
 )
